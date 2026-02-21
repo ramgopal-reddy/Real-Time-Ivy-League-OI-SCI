7a508c86c81c5ec3a1cfe02bb620590b5651a9ab
Ram Gopal Reddy <redabothularamgopalreddy@gmail.com>
2026-02-21 23:00:43 +0530

    Create models.py

---
+++ models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, Text
+from database import Base
+
+class Opportunity(Base):
+    __tablename__ = "opportunities"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    university = Column(String, index=True)
+    domain = Column(String)
+    sub_domain = Column(String)
+    deadline = Column(String)
+    eligibility = Column(Text)
+    skills_required = Column(Text)
+    application_link = Column(String)
